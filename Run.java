892a1f3617131d3520201d6ed6432b5ef8c43269
cogmlek12-afk <cogmlek12@gmail.com>
2026-06-21 19:49:40 +0900

    Create Run.java

---
+++ Run.java
@@ -0,0 +1,13 @@
+public class Run {
+
+    public static void main(String[] args) {
+
+
+        Cosmetic product =
+                new Cosmetic("립스틱", "코스메틱A", 15000);
+
+
+        product.showInfo();
+
+    }
+}
